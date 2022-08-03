cfa9a070625ae910dfb6f1e1176441145c11ad43
Mostafa <waheedmostafa00@gmail.com>
2022-08-03 22:19:02 +0200

    Finalize the setup of the app

---
+++ src/app/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../assets/styles';
+
+function App() {
+    return (<h1>Welcome to Jobzilla!</h1>);
+}
+
+export default App;
---
+++ src/assets/styles.js
@@ -0,0 +1,6 @@
+// import required styles and configurations
+import '../../node_modules/bootstrap/scss/bootstrap.scss';
+import './styles/app.scss';
+
+// import required js style files
+import '../../node_modules/bootstrap/dist/js/bootstrap.bundle';
